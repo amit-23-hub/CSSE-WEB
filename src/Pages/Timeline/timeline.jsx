a7f284abf183d346f959df22f3832d36d3bcc1a8
Akash <akashgupta7484@gmail.com>
2024-09-24 23:40:24 +0530

    removed img and button from timeline

--- src/Pages/Timeline/timeline.jsx
+++ src/Pages/Timeline/timeline.jsx
@@ -13,8 +13,6 @@ const Timeline = () => {
       >
         <h3>Event Title 1</h3>
         <p>Description for event 1.</p>
-        <img src="image1.jpg" alt="Event 1" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         date="2024-02-01"
@@ -23,8 +21,6 @@ const Timeline = () => {
       >
         <h3>Event Title 2</h3>
         <p>Description for event 2.</p>
-        <img src="image2.jpg" alt="Event 2" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         date="2024-03-01"
@@ -33,8 +29,6 @@ const Timeline = () => {
       >
         <h3>Event Title 3</h3>
         <p>Description for event 3.</p>
-        <img src="image3.jpg" alt="Event 3" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         date="2024-04-01"
@@ -43,8 +37,6 @@ const Timeline = () => {
       >
         <h3>Event Title 4</h3>
         <p>Description for event 4.</p>
-        <img src="image4.jpg" alt="Event 4" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         date="2024-05-01"
@@ -53,8 +45,6 @@ const Timeline = () => {
       >
         <h3>Event Title 5</h3>
         <p>Description for event 5.</p>
-        <img src="image5.jpg" alt="Event 5" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         date="2024-06-01"
@@ -63,8 +53,6 @@ const Timeline = () => {
       >
         <h3>Event Title 6</h3>
         <p>Description for event 6.</p>
-        <img src="image6.jpg" alt="Event 6" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         date="2024-07-01"
@@ -73,8 +61,6 @@ const Timeline = () => {
       >
         <h3>Event Title 7</h3>
         <p>Description for event 7.</p>
-        <img src="image7.jpg" alt="Event 7" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         date="2024-08-01"
@@ -83,8 +69,6 @@ const Timeline = () => {
       >
         <h3>Event Title 8</h3>
         <p>Description for event 8.</p>
-        <img src="image8.jpg" alt="Event 8" className="event-image" />
-        <button className="view-more-button">View More</button>
       </VerticalTimelineElement>
     </VerticalTimeline>
   );
