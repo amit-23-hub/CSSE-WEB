129435d3131224ef5a56dfbfa311232374b00f15
Akash <akashgupta7484@gmail.com>
2024-10-02 17:25:49 +0530

    home

--- src/Pages/HomeContent/HomeContent.jsx
+++ src/Pages/HomeContent/HomeContent.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import './HomeContent.css';
-import img1 from '../../assets/planet1.png'
+import edTech from '../../assets/edTech.png'
 import Gallery from "../triCard/triCard";
 import FaqComponent from "../FAQ/Faqs";
+import LOGOCSSE from '../../assets/LOGOCSSE.png'
+
 const Section = () => {
   return (
     <div className="container">
@@ -19,24 +21,31 @@ const Section = () => {
 </svg>
 
 <section className="col2 color ss-style-curvedown overflow-hidden flex flex-col-reverse md:flex-row items-center justify-between p-8">
-  <div className="w-full md:w-1/2 mb-6 md:mb-0 text-white">
-    <h2 className="text-3xl font-bold mb-4">About CSSE</h2>
-    <p className="text-lg ml-10">
-      About the society or description goes here. This is where you can add more details about the society or event.
-      About the society or description goes here. This is where you can add more details about the society or event.
-      About the society or description goes here. This is where you can add more details about the society or event.
+<div className="w-full md:w-1/2 flex justify-center md:justify-end mb-6 md:mb-0">
+    <div className="logo-div relative overflow-hidden hover:scale-110 transition-transform duration-300 ease-in-out mr-0 md:mr-60">
+      <img
+        src={LOGOCSSE}
+        alt="Hexagonal Image"
+        className="w-60 h-70 md:w-70 md:h-70 object-cover"
+      />
+    </div>
+</div>
+  
+  <div className="w-full md:w-1/2 mb-6 md:mb-0 text-white mr-20">
+    <h2 className="text-3xl font-bold mb-4 underline">About CSSE</h2>
+    <p className="text-lg ml-10 text-left">
+    ▪ Founded in the year 2013, by Alumni Respected Ankit Porwal and Vivek Samele.<br/>
+    ▪ The basic OBJECTIVES of the society are:<br/>
+    ▪ to develop the managerial and soft skills of the students<br/>
+    ▪ to organize activities for social awareness and social benefits.<br/>
+    ▪ Toincrease mutual bonding among each other.<br/>
+    ▪ The Society organizes Events(Formal and Informal)for the students of all MCA & B.
+      Tech (IT) Students.<br/>
+    ▪ The Society organizes personality development programs such as seminars, 
+      workshops, and Language classes for the students to learn from their seniors.
     </p>
   </div>
 
-  <div className="w-full md:w-1/2 flex justify-center md:justify-end mb-6 md:mb-0">
-    <div className="relative hexagon overflow-hidden hover:scale-110 transition-transform duration-300 ease-in-out mr-0 md:mr-20">
-      <img
-        src={img1}
-        alt="Hexagonal Image"
-        className="w-70 h-70 md:w-70 md:h-70 object-cover"
-      />
-    </div>
-  </div>
 </section>
 
 <svg
@@ -57,20 +66,21 @@ const Section = () => {
         <path d="M0 100 C 20 0 50 0 100 100 Z" />
       </svg>
       <section className="color overflow-hidden flex flex-col md:flex-row items-center justify-between">
-  <div className="md:w-1/2 w-full p-4">
-    <h2 className="text-2xl font-bold mb-4">Our Vision</h2>
-    <p className="text-lg">
-      Our vision is to empower individuals and organizations by providing high-quality services that foster growth, innovation, and sustainability. We aim to be leaders in our field, setting benchmarks in excellence and contributing positively to society.
-    </p>
-  </div>
-  <div className="md:w-1/2 w-full p-4">
-    <img 
-      src={img1} 
-      alt="Vision" 
-      className="w-full h-auto object-cover rounded-lg shadow-lg"
-    />
-  </div>
-</section>
+        <div className="md:w-1/2 w-full p-4  hover:scale-105 transition-transform duration-300 ease-in-out">
+          <img 
+            src={edTech} 
+            alt="Vision" 
+            className="w-full h-auto object-cover rounded-lg shadow-lg"
+          />
+        </div>
+        <div className="md:w-1/2 w-full p-4">
+          <h2 className="text-2xl font-bold mb-4">Our Vision</h2>
+          <p className="text-lg">
+            Our vision is to empower individuals and organizations by providing high-quality services that foster growth, innovation, and sustainability. We aim to be leaders in our field, setting benchmarks in excellence and contributing positively to society.
+          </p>
+        </div>
+  
+      </section>
 <svg
   id="curvedowncolor"
   xmlns="http://www.w3.org/2000/svg"
--- src/Pages/triCard/triCard.jsx
+++ src/Pages/triCard/triCard.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import gallery1 from '../../assets/gallery1.jpg'
+import gallery2 from '../../assets/gallery2.jpg'
+import gallery3 from '../../assets/gallery3.jpg'
+import gallery4 from '../../assets/gallery4.jpg'
+import gallery5 from '../../assets/gallery5.jpg'
 
+
+
+ 
 const Gallery = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -34,25 +42,19 @@ const Gallery = () => {
     <div className="slider-container mx-auto w-full">
       <Slider {...settings}>
         <div className="p-2">
-          <img className="w-full h-48 object-cover" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5_TrWm1I247bI2a2Hifre2DlpSJFGFtfvnA&s" alt="Slide 1" />
+          <img className="w-full h-48 object-cover rounded-xl" src={gallery1} alt="Slide 1" />
         </div>
         <div className="p-2">
-          <img className="w-full h-48 object-cover" src="https://hire4event.com/blogs/wp-content/uploads/2019/02/hire4event.com_-1.jpg" alt="Slide 2" />
+          <img className="w-full h-48 object-cover rounded-xl" src={gallery2} alt="Slide 2" />
         </div>
         <div className="p-2">
-          <img className="w-full h-48 object-cover" src="https://www.ticketleap.com/wp-content/uploads/2024/03/fbc316b9-10b3-4dcb-a9f2-8092d5c00d94.png" alt="Slide 3" />
+          <img className="w-full h-48 object-cover rounded-xl" src={gallery3} alt="Slide 3" />
         </div>
         <div className="p-2">
-          <img className="w-full h-48 object-cover" src="https://artisthubhq.com/wp-content/uploads/2018/04/ultimate-guide.jpg" alt="Slide 4" />
+          <img className="w-full h-48 object-cover rounded-xl" src={gallery4} alt="Slide 4" />
         </div>
         <div className="p-2">
-          <img className="w-full h-48 object-cover" src="https://www.yakubevents.com/assets/images/college_events/ce1.jpg" alt="Slide 5" />
-        </div>
-        <div className="p-2">
-          <img className="w-full h-48 object-cover" src="https://hire4event.com/blogs/wp-content/uploads/2019/02/hire4event.com_-1.jpg" alt="Slide 6" />
-        </div>
-        <div className="p-2">
-          <img className="w-full h-48 object-cover" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5_TrWm1I247bI2a2Hifre2DlpSJFGFtfvnA&s" alt="Slide 1" />
+          <img className="w-full h-48 object-cover rounded-xl" src={gallery5} alt="Slide 5" />
         </div>
       </Slider>
     </div>
