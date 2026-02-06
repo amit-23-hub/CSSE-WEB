25519de486589a1d8cbaabcb240fe343b7e194c3
Akash Gupta <akashgupta7484@gmail.com>
2026-02-06 23:59:40 +0530

    rate limiter fixed with mail function

--- server/config/emailService.js
+++ server/config/emailService.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
     port: process.env.EMAIL_PORT || 587,
     secure: false,
